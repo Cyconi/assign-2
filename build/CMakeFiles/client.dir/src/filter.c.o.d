580c38ceb59cf85deb0852302aefd4fc2c4a128a
jonathan <jonhilde5@gmail.com>
2024-10-11 18:59:01 -0700

    t

---
+++ build/CMakeFiles/client.dir/src/filter.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/client.dir/src/filter.c.o: \
+  /home/jonathan/comp-3980-assign-2/src/filter.c \
+  /home/jonathan/comp-3980-assign-2/src/../include/filter.h
---
+++ build/CMakeFiles/server.dir/src/filter.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/server.dir/src/filter.c.o: \
+  /home/jonathan/comp-3980-assign-2/src/filter.c \
+  /home/jonathan/comp-3980-assign-2/src/../include/filter.h
